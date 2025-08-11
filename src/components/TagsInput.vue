6d2a090212cd01848324224cf0f8e3a6d6a2d203
darfianardiansyah <darfianardiansyah@gmail.com>
2025-08-11 11:28:08 +0700

    Styling the Tags Input component

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,13 +1,21 @@
 <template>
-  <div v-for="(tag, index) in tags" :key="index">
-    {{ index + " : " + tag }}
-    <a @click.prevent="removeTag(index)" href="#">x</a>
+    <div class="tags-input-wrapper">
+    <span class="tag-item" v-for="(tag, index) in tags" :key="index">
+      {{ tag }}
+      <a class="remove-tag" @click.prevent="removeTag(index)" href="#"
+        >&times;</a
+      >
+    </span>
+    <input
+      class="tag-input"
+      type="text"
+      v-model.trim="newTag"
+      @keydown.enter="addNewTag"
+      @keydown.delete="removeLastTag"
+      @keydown.tab.prevent="addNewTag"
+      :class="{ 'tag-exists': isTagExists }"
+    />
   </div>
-  <!-- <hr />
-  {{ newTag }} -->
-  <input type="text" v-model.trim="newTag" @keydown.enter="addNewTag" @keydown.delete="removeLastTag"
-    @keydown.tab.prevent="addNewTag"
-    :class="{ 'tag-exists': isTagExists }"/>
 </template>
 
 <script>
@@ -17,17 +25,17 @@ export default {
     newTag: "",
   }),
   watch: {
-    newTag(newVal){
-      if(newVal.indexOf(",")>-1){
-        this.newTag = this.newTag.slice(0,-1);
+    newTag(newVal) {
+      if (newVal.indexOf(",") > -1) {
+        this.newTag = this.newTag.slice(0, -1);
         this.addNewTag();
       }
-    }
+    },
   },
   computed: {
     isTagExists() {
       return this.tags.includes(this.newTag);
-    }
+    },
   },
   methods: {
     addNewTag() {
@@ -40,20 +48,55 @@ export default {
       this.tags.splice(index, 1);
     },
     removeLastTag() {
-      if (this.tags.length > 0) {
-        this.tags.pop();
+      if (this.newTag.length === 0) {
+        this.removeTag(this.tags.length - 1);
       }
-    }
+    },
   },
 };
 </script>
 <style scoped>
-button {
-  background-color: rgb(42, 95, 42);
-  color: white;
-}
-.tag-exists {
-  color:red;
+.tag-input.tag-exists {
+  color: red;
   text-decoration: line-through;
+}
+.tags-input-wrapper {
+  background: #fff;
+  padding: 0.5em;
+  border: 1px solid #dbdbdb;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  min-height: 36px;
+  box-sizing: border-box;
+}
+
+.tag-item {
+  color: #212529;
+  background-color: #eee;
+  margin-right: 0.3em;
+  padding: 0.25em 0.4em;
+  font-size: 75%;
+  line-height: 1;
+  text-align: center;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-right: 1.25em;
+  padding-left: 0.6em;
+}
+
+.tag-input {
+  color: #495057;
+  flex: 1;
+  background: transparent;
+  border: none;
+}
+
+.tag-input:focus {
+  outline: none;
+}
+a.remove-tag {
+  text-decoration: none;
 }
 </style>
