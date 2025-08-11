8f81a9fe4c96d7db30eab080d13b23156eda1183
darfianardiansyah <darfianardiansyah@gmail.com>
2025-08-11 11:37:31 +0700

    Component Communication with Props

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
-// import TagsInput from './components/TagsInput.vue';
-// export default {
-//   components: {
-//     TagsInput
-//   }
-// }
+export default {
+  data: () => ({
+    jsFrameworks: ["react.js", "vue.js", "angular"],
+  }),
+  mounted() {
+    setTimeout(() => {
+      this.jsFrameworks.push("svelte.js");
+    }, 3000);
+  },
+};
 </script>
 
 <template>
   <h1>Hello World</h1>
-  <tags-input />
+  <tags-input :selected-tags="jsFrameworks" />
 </template>
 
 <style>
